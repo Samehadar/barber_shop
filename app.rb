7d6405ef70ac21ac704dcb93929220f8645555b5
Vitaly <stels95@list.ru>
2016-08-31 18:34:55 +0600

    add action on contacts and visit pages

--- app.rb
+++ app.rb
@@ -18,3 +18,33 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	username = params[:username]
+	phone = params[:phone]
+	date_time = params[:date_time]
+	
+	file = File.open "./public/users.txt", "a"
+	file.write "{\"username\": \"#{username}\"; \"phone\": \"#{phone}\"; \"date_time\": \"#{date_time}\"}\n"
+	file.close 
+
+	@message = "#{username}, we will waiting for you at #{date_time}!"
+	@title = "Thank you for visit!"
+
+	erb :message
+
+end
+
+post '/contacts' do
+	email =  params[:email]
+	text = params[:text_area]
+
+	file = File.open "./public/contacts.txt", "a"
+	file.write "{\"email\": \"#{email}\"; \"text\": \"#{text}\"}\n"
+	file.close 
+
+	@message = "We send your email to BarberShop mail!"
+	@title = "Thank you for your message!"
+
+	erb :message
+end
